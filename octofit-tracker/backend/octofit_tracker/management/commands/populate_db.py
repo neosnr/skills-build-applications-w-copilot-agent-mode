da1836ae024da2b7828fff2afbabaf04dec1b879
neosnr <sivn393@gmail.com>
2026-04-03 11:18:15 +0000

    Add populate_db management command for test data population

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,40 @@
+from django.core.management.base import BaseCommand
+from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **kwargs):
+        # Clear existing data
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Create teams
+        marvel = Team.objects.create(name='marvel', description='Marvel Superheroes')
+        dc = Team.objects.create(name='dc', description='DC Superheroes')
+
+        # Create users
+        tony = User.objects.create(name='Tony Stark', email='tony@marvel.com', team=marvel.name)
+        steve = User.objects.create(name='Steve Rogers', email='steve@marvel.com', team=marvel.name)
+        bruce = User.objects.create(name='Bruce Wayne', email='bruce@dc.com', team=dc.name)
+        clark = User.objects.create(name='Clark Kent', email='clark@dc.com', team=dc.name)
+
+        # Create activities
+        Activity.objects.create(user=tony, activity_type='run', duration=30, date='2023-01-01')
+        Activity.objects.create(user=steve, activity_type='cycle', duration=45, date='2023-01-02')
+        Activity.objects.create(user=bruce, activity_type='swim', duration=60, date='2023-01-03')
+        Activity.objects.create(user=clark, activity_type='yoga', duration=20, date='2023-01-04')
+
+        # Create workouts
+        Workout.objects.create(name='Pushups', description='Do 20 pushups', difficulty='easy')
+        Workout.objects.create(name='Squats', description='Do 30 squats', difficulty='medium')
+        Workout.objects.create(name='Plank', description='Hold plank for 1 min', difficulty='hard')
+
+        # Create leaderboard
+        Leaderboard.objects.create(team=marvel, points=150)
+        Leaderboard.objects.create(team=dc, points=120)
+
+        self.stdout.write(self.style.SUCCESS('Database populated with test data.'))
